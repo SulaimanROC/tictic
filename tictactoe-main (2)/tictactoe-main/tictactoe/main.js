45972ca353dcc683e44f1cf08a700830e0c379c5
Sulaiman Hussain <118259411+SulaimanROC@users.noreply.github.com>
2022-11-29 10:21:13 +0100

    Add files via upload

---
+++ tictactoe-main (2)/tictactoe-main/tictactoe/main.js
@@ -0,0 +1,9 @@
+console.log("App is running...");
+
+function myFunction() {
+    alert('hey');
+   
+}
+
+const tic = document.querySelector('.container');
+tic.addEventListener('click', myFunction);
